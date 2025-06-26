88e24d6ac2963c93da0e7d2af0f938c2ae1a6603
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2025-06-26 16:46:26 +0800

    新增 Tailwind CSS 整合，更新相依套件，並新增按鈕元件及工具函式以支援樣式合併。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [
+    preact({
+      // devtools: true,
+      compat: true, // 使用 preact 的相容性模式
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
